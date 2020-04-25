8afe0e3e4a7922ee78638d8c3ac964d376bdfdcb
adhaar-star <59237202+adhaar-star@users.noreply.github.com>
2020-04-24 21:03:44 -0700

    Adding Unit tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -24,6 +24,75 @@ TEST(PracticeTest, is_simple_palindrome)
 TEST(PracticeTest, is_not_a_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("Ba");
+    bool actual = obj.isPalindrome("ba");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_not_alphabet_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("--");
     ASSERT_TRUE(actual);
+}
+
+
+TEST(PracticeTest, is_single_alphabet_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("t");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_empty_string_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(" ");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_upper_lower_string_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Cc");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_string_with_space_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Cd Dc");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_number_with_word_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("12abba21"); 
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_order_incorrect)
+{
+	Practice obj;
+	int a = 45;
+	int b = 46;
+	int c = 47;
+	obj.sortDescending(a, b, c);
+    ASSERT_LT(c, a);
+	ASSERT_LT(b, a);
+	ASSERT_LT(c, b);
+
+}
+
+TEST(PracticeTest, is_order_correct)
+{
+	Practice obj;
+	int a = 99;
+	int b = 98;
+	int c = 96;
+	obj.sortDescending(a, b, c);
+    ASSERT_LT(c, a);
+	ASSERT_LT(b, a);
+	ASSERT_LT(c, b);
+
 }
